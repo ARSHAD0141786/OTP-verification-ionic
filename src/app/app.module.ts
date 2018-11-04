0dace9de1c5f156c1a42a18d1f74304b40451ee8
MOHAMMED ARSHAD <arshad0141786@gmail.com>
2018-11-04 13:40:03 +0530

    send function completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-
+import * as firebase from 'firebase';
 @NgModule({
   declarations: [
     MyApp,
@@ -14,7 +14,7 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,22 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  verificationID:any;
+  otpCode:String = '';
   constructor(public navCtrl: NavController) {
 
   }
 
+  send(){
+    console.log('Sending OTP......');
+    (<any>window).FirebasePlugin.verifyPhoneNumber('+918441975563', 120,(credential)=>{
+      console.log('Credentials : '+credential);
+    }, (error)=>{
+      console.log(error);
+    });
+  }
+
+  verifyOTP(){
+
+  }
 }
